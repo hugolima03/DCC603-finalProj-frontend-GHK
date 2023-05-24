7c3ef3cbd6e0b85f7cc437b62b05f714619999ea
Hugo Lima Romão <hugo8romao@gmail.com>
2023-05-24 11:26:22 -0400

    chore: storybook preview setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,13 @@
 import type { Preview } from '@storybook/react'
+import { withThemeFromJSXProvider } from '@storybook/addon-styling';
+
+import { GlobalStyle } from "../src/styles/global";
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    GlobalStyles: GlobalStyle,
+  }),
+];
 
 const preview: Preview = {
   parameters: {
