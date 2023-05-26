26e2b34324f47b962ae633682d1a5f76f827d44f
Hugo Lima Romão <hugo8romao@gmail.com>
2023-05-26 13:27:34 -0400

    feat: classes query

---
+++ src/hooks/useClasses.tsx
@@ -0,0 +1,33 @@
+import useSWR from 'swr'
+import fetcher from 'utils/fetcher'
+
+export interface ClassResponse {
+  data: Class[]
+  error: any
+}
+
+export interface Class {
+  slug: string
+  course_name: string
+  course_photo: string
+  course_workload: number
+  start_date: string
+  end_date: string
+  teacher: string
+  course_category: string
+}
+
+function useClasses() {
+  const { data, error, isLoading } = useSWR<ClassResponse>(
+    `http://localhost:5050/classes`,
+    fetcher
+  )
+
+  return {
+    classes: data?.data,
+    isLoading,
+    isError: data?.error || error
+  }
+}
+
+export default useClasses
